e8636795d040c342d4fef5f40bd1186ef76783cb
kitfox <kitfox@7dc7fa77-23fb-e6ad-8e2e-c86bd48ed22b>
2013-01-11 08:59:49 +0000

    Updating headers to reflect BSD license.
    git-svn-id: https://svn.java.net/svn/svgsalamander~svn/trunk@143 7dc7fa77-23fb-e6ad-8e2e-c86bd48ed22b

--- svg-example/src/com/kitfox/svg/example/SimpleIconFrame.java
+++ svg-example/src/com/kitfox/svg/example/SimpleIconFrame.java
@@ -1,3 +1,39 @@
+/*
+ * SVG Salamander
+ * Copyright (c) 2004, Mark McKay
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *   - Redistributions of source code must retain the above 
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer.
+ *   - Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials 
+ *     provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * 
+ * Mark McKay can be contacted at mark@kitfox.com.  Salamander and other
+ * projects can be found at http://www.kitfox.com
+ *
+ * Created on Mar 10, 2010, 11:25:47 AM
+ */
+
 package com.kitfox.svg.example;
 
 import com.kitfox.svg.app.beans.SVGPanel;
@@ -5,17 +41,6 @@ import java.awt.BorderLayout;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * SimpleIconFrame.java
- *
- * Created on Mar 10, 2010, 11:25:47 AM
- */
 
 /**
  *
@@ -33,8 +58,8 @@ public class SimpleIconFrame extends javax.swing.JFrame
         getContentPane().add(panel, BorderLayout.CENTER);
         panel.setAntiAlias(true);
         try {
-//            panel.setSvgURI(SimpleIconFrame.class.getResource("tree.svg").toURI());
-            panel.setSvgURI(SimpleIconFrame.class.getResource("markerEnd.svg").toURI());
+            panel.setSvgURI(SimpleIconFrame.class.getResource("tree.svg").toURI());
+//            panel.setSvgURI(SimpleIconFrame.class.getResource("markerEnd.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("casing2.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("casing2-opt.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("PseudoColor.svg").toURI());
