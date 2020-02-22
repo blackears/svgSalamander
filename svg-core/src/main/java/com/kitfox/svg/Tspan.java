83127824c3af92469db5d6355c04246d6e4396dd
Stefan Niederhauser <ghuder5@gmx.ch>
2020-02-22 21:46:28 +0100

    decimal font size

--- svg-core/src/main/java/com/kitfox/svg/Tspan.java
+++ svg-core/src/main/java/com/kitfox/svg/Tspan.java
@@ -230,14 +230,14 @@ public class Tspan extends ShapeElement
         Font font = diagram.getUniverse().getFont(fontFamily);
         if (font == null && fontFamily != null)
         {
-            font = FontSystem.createFont(fontFamily, fontStyle, fontWeight, (int)fontSize);
+            font = FontSystem.createFont(fontFamily, fontStyle, fontWeight, fontSize);
 //            addShapeSysFont(addShape, font, fontFamily, fontSize, letterSpacing, cursor);
 //            return;
         }
 
         if (font == null)
         {
-            font = FontSystem.createFont("Serif", fontStyle, fontWeight, fontStyle);
+            font = FontSystem.createFont("Serif", fontStyle, fontWeight, fontSize);
         }
 
 //        FontFace fontFace = font.getFontFace();
