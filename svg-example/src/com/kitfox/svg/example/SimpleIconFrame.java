2b847cfca317b3bcc32feb3b1deb2af53ca6d326
Mark McKay <mark@kitfox.com>
2020-03-01 11:34:11 -0500

    Removing test.

--- svg-example/src/com/kitfox/svg/example/SimpleIconFrame.java
+++ svg-example/src/com/kitfox/svg/example/SimpleIconFrame.java
@@ -75,7 +75,6 @@ public class SimpleIconFrame extends javax.swing.JFrame
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("notes.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("jira51-textLength.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("jira50-display.svg").toURI());
-//            panel.setSvgURI(SimpleIconFrame.class.getResource("jira44-antialias.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("bigMarkers.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("radialGradient.svg").toURI());
 //            panel.setSvgURI(SimpleIconFrame.class.getResource("jira43-tspanVariableX.svg").toURI());
